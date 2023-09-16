4cafe600f4466a9b835766f2bf43acc5afc4a950
Pandeigede2803 <131873435+Pandeigede2803@users.noreply.github.com>
2023-09-16 13:01:00 +0800

    edit responsive dahsboard

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,21 +1,106 @@
 import React, { useState } from "react";
 import Link from "next/link";
-import { FaHome, FaEdit, FaUser, FaCog } from "react-icons/fa";
+import { FaHome, FaEdit, FaUser, FaCog, FaBars } from "react-icons/fa";
 import PortofolioForm from "@/components/layouts/PortofolioForm/page";
 import PortofolioTable from "@/components/layouts/PortofolioTable/page";
 import Setting from "@/components/layouts/setting/page";
 import EditProfile from "@/components/layouts/EditProfile/page";
 
 const Dashboard = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   // Use state to manage which component to render
   const [displayForm, setDisplayForm] = useState(false);
   const [displaySetting, setDisplaySetting] = useState(false); // State for rendering Setting
   const [displayEditProfile, setDisplayEditProfile] = useState(false); // State for rendering EditProfile
 
   return (
-    <div className="flex h-full  ">
+    //create full code of hamburger here
+
+    <div className="flex flex-col h-full  ">
+      <div className="bg-[#A28BFF]">
+        {/* Hamburger Icon */}
+        <button
+          className="lg:hidden block"
+          onClick={toggleMobileMenu}
+          aria-label="Toggle mobile menu"
+        >
+          <FaBars className="text-2xl text-gray-800  " />
+        </button>
+
+        {/* Mobile Navbar Dropdown */}
+        {isMobileMenuOpen && (
+          <div className=" bg-[#A28BFF]  shadow-lg min-h-max lg:hidden">
+            <div className="p-4">
+              <h1 className="text-2xl font-semibold text-gray-800">
+                DASHBOARD
+              </h1>
+            </div>
+            <ul className="mt-4">
+              <li className="mb-2">
+                <a
+                  href="#"
+                  className="block px-4 py-2 text-gray-800 hover:bg-gray-200"
+                  onClick={() => {
+                    setDisplayForm(false);
+                    setDisplaySetting(false);
+                    setDisplayEditProfile(false);
+                  }}
+                >
+                  <FaHome className="inline-block mr-2" /> HOME
+                </a>
+              </li>
+              <li className="mb-2">
+                <a
+                  href="#"
+                  className="block px-4 py-2 text-gray-800 hover:bg-gray-200"
+                  onClick={() => {
+                    setDisplayForm(true);
+                    setDisplaySetting(false);
+                    setDisplayEditProfile(false);
+                  }}
+                  
+                >
+                  <FaEdit className="inline-block mr-2" /> POST
+                </a>
+              </li>
+              <li className="mb-2">
+                <a
+                  href="#"
+                  className="block px-4 py-2 text-gray-800 hover:bg-gray-200"
+                  onClick={() => {
+                    setDisplayForm(false);
+                    setDisplaySetting(false);
+                    setDisplayEditProfile(true);
+                  }}
+                
+                >
+                  <FaUser className="inline-block mr-2" /> USER
+                </a>
+              </li>
+              <li className="mb-2">
+                <a
+                  href="#"
+                  className="block px-4 py-2 text-gray-800 hover:bg-gray-200"
+                  onClick={() => {
+                    setDisplayForm(false);
+                    setDisplaySetting(true);
+                    setDisplayEditProfile(false);
+                  }}
+                >
+                  <FaCog className="inline-block mr-2" /> SETTINGS
+                </a>
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
       {/* Sidebar */}
-      <div className="w-64 bg-[#A28BFF] shadow-lg min-h-max">
+      <div className="w-64 bg-[#A28BFF] shadow-lg min-h-max hidden lg:block ">
         <div className="p-4 ">
           <h1 className="text-2xl font-semibold text-gray-800">Dashboard</h1>
         </div>
