83b22fb076ee2104233a2213a307ef616e40e6ac
Pandeigede2803 <131873435+Pandeigede2803@users.noreply.github.com>
2023-09-20 10:19:32 +0800

    edit front end link

--- src/utils/Config.js
+++ src/utils/Config.js
@@ -6,4 +6,4 @@ export const backendUrl = "https://api.porto2.bossprojec.com";
 
 //post and get url
 
-export const postgetUrl = "https://api.porto2.bossprojec.com/api/projects";
+export const postgetUrl = "https://api.porto2.bossprojec.com/api/projects";;
