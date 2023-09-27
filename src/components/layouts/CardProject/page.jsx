ad623a2cb775e39ad21760bdf7f1147db8e849b5
Pandeigede2803 <131873435+Pandeigede2803@users.noreply.github.com>
2023-09-27 15:20:21 +0800

    animation card

--- src/components/layouts/CardProject/page.jsx
+++ src/components/layouts/CardProject/page.jsx
@@ -4,9 +4,9 @@ export default function CardProject(props) {
   const { imageSrc, company, title, description } = props;
 
   return (
-    <div className="relative flex flex-col justify-center my-9 lg:mx-2   ">
+    <div className="relative flex flex-col justify-center my-9 lg:mx-2  ">
       {/* <!-- card --> */}
-      <div className=" my-auto mx-auto flex w-96  flex-col justify-center bg-white rounded-2xl shadow-l shadow-slate-300/60">
+      <div className=" hover:opacity-70 my-auto mx-auto flex w-96  flex-col justify-center bg-white rounded-2xl shadow-l shadow-slate-300/60">
         {/* <!-- img --> */}
         <img
           className="aspect-video w-96 rounded-t-2xl object-cover object-center"
